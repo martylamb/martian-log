a68e8373af0c7e3282df80b519d1f8e375e944e6
Marty Lamb <mlamb@martiansoftware.com>
2019-08-07 20:24:59 -0400

    added license headers

--- src/main/java/com/martiansoftware/log/CallerOf.java
+++ src/main/java/com/martiansoftware/log/CallerOf.java
@@ -1,20 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.martiansoftware.log;
 
+//   Copyright 2017 Martian Software, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 /**
- *
- * @author mlamb
+ * Utility class used to peek up the stack at its invoker
+ * 
+ * @author <a href="http://martylamb.com">Marty Lamb</a>
  */
-public class CallerOf {
-    private final static SecurityManager mySecurityManager = new SecurityManager();
+class CallerOf {
+    private final static SecurityManager MY_SECURITY_MANAGER = new SecurityManager();
 
     public static Class stackAncestor(int generationsBack) {
         if (generationsBack < 0) throw new IllegalArgumentException("CallingClass.get() requires an offset >= 0");
-        return mySecurityManager.getCallingClass(generationsBack);
+        return MY_SECURITY_MANAGER.getCallingClass(generationsBack);
     }
     
     public static Class thisMethod() {
--- src/main/java/com/martiansoftware/log/StopwatchLog.java
+++ src/main/java/com/martiansoftware/log/StopwatchLog.java
@@ -1,14 +1,29 @@
 package com.martiansoftware.log;
 
+//   Copyright 2017 Martian Software, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 import java.time.Duration;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import org.slf4j.Logger;
 
 /**
- *
- * @author mlamb
+ * A Log that measures elapsed time between its creating and its closing.
+ * A StopwatchLog can log warnings or errors if user-set thresholds are exceeded.
+ * 
+ * @author <a href="http://martylamb.com">Marty Lamb</a>
  */
 public class StopwatchLog extends Log implements AutoCloseable {
     
@@ -29,11 +44,27 @@ public class StopwatchLog extends Log implements AutoCloseable {
         return name == null ? "Stopwatch" : name;
     }
     
+    /**
+     * Specifies a Duration that will be compared against the elapsed time
+     * when the StopwatchLog is closed.  If the Duration is exceeded, a warning
+     * will be logged.
+     * 
+     * @param warnThreshold
+     * @return this StopwatchLog
+     */
     public StopwatchLog warnOver(Duration warnThreshold) {
         _warnThreshold = warnThreshold;
         return this;
     }
     
+    /**
+     * Specifies a Duration that will be compared against the elapsed time
+     * when the StopwatchLog is closed.  If the Duration is exceeded, an error
+     * will be logged.
+     * 
+     * @param errorThreshold
+     * @return this StopwatchLog
+     */
     public StopwatchLog errorOver(Duration errorThreshold) {
         _errorThreshold = errorThreshold;
         return this;
